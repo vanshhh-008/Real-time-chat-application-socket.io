ef449831d01e1f6120e220884f6e1855778e68bc
Vansh.Saraswat <vanshsaraswat348@gmail.com>
2025-06-14 17:53:47 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const server = app.listen(9000,console.log(`Server started on port ${PORT}`));
 const io = require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://real-chatify.netlify.app/",
 
     }
 })
